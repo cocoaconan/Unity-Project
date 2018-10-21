40166394dc45563740f4d94370383e5c3b6c7226
cocoaconan <43486166+cocoaconan@users.noreply.github.com>
2018-10-21 14:16:12 +1300

    testing

---
+++ Clicker Game/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stat health;
+    [SerializeField]
+    private Stat energy;
+
+    private void Awake()
+    {
+        health.Initialize();
+        energy.Initialize();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            health.CurrentVal += 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            energy.CurrentVal -= 10;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            energy.CurrentVal += 10;
+        }
+    }
+}
---
+++ RPG Clicker/Assets/Scenes/README.md
@@ -0,0 +1,2 @@
+# RPG-Clicker-S53
+Contains the project for the Clicker game, Software Development Practice Project.
